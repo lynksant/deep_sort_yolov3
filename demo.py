38727fb0693f265504cc45bc2a5f06496e9e9a92
Leonid Kahanovskyi <lynksant@gmail.com>
2020-06-20 03:24:37 -0700

    Optimization, track only confirmed instances + modifications to run on Collab

--- demo.py
+++ demo.py
@@ -25,19 +25,19 @@ def main(yolo):
     # Definition of the parameters
     max_cosine_distance = 0.3
     nn_budget = None
-    nms_max_overlap = 1.0
-    
+    nms_max_overlap = 2.0
+
     # Deep SORT
     model_filename = 'model_data/mars-small128.pb'
     encoder = gdet.create_box_encoder(model_filename,batch_size=1)
-    
+
     metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
-    tracker = Tracker(metric)
+    tracker = Tracker(metric, max_age=150, n_init=3)
 
     writeVideo_flag = True
     asyncVideo_flag = False
 
-    file_path = 'video.webm'
+    file_path = 'input/orig_video.mp4'
     if asyncVideo_flag :
         video_capture = VideoCaptureAsync(file_path)
     else:
@@ -54,11 +54,14 @@ def main(yolo):
             w = int(video_capture.get(3))
             h = int(video_capture.get(4))
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        out = cv2.VideoWriter('output_yolov3.avi', fourcc, 30, (w, h))
+        out = cv2.VideoWriter('output/output_yolov3.avi', fourcc, 30, (w, h))
         frame_index = -1
 
     fps = 0.0
     fps_imutils = imutils.video.FPS().start()
+
+    track_count = 0
+    unique_track_ids_set = set()
 
     while True:
         ret, frame = video_capture.read()  # frame shape 640*480*3
@@ -68,37 +71,39 @@ def main(yolo):
         t1 = time.time()
 
         image = Image.fromarray(frame[...,::-1])  # bgr to rgb
-        boxs = yolo.detect_image(image)[0]
-        confidence = yolo.detect_image(image)[1]
+        detected_image = yolo.detect_image(image)
+        boxs = detected_image[0]
+        confidence = detected_image[1]
 
         features = encoder(frame,boxs)
 
         detections = [Detection(bbox, confidence, feature) for bbox, confidence, feature in zip(boxs, confidence, features)]
-        
+
         # Run non-maxima suppression.
         boxes = np.array([d.tlwh for d in detections])
         scores = np.array([d.confidence for d in detections])
         indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
         detections = [detections[i] for i in indices]
-        
+
         # Call the tracker
         tracker.predict()
         tracker.update(detections)
-        
+
         for track in tracker.tracks:
             if not track.is_confirmed() or track.time_since_update > 1:
-                continue 
+                continue
+            unique_track_ids_set.add(track.track_id)
+            track_count = len(unique_track_ids_set)
+
             bbox = track.to_tlbr()
-            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
-            cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
 
         for det in detections:
             bbox = det.to_tlbr()
             score = "%.2f" % round(det.confidence * 100, 2)
             cv2.rectangle(frame,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,0,0), 2)
             cv2.putText(frame, score + '%', (int(bbox[0]), int(bbox[3])), 0, 5e-3 * 130, (0,255,0),2)
-            
-        cv2.imshow('', frame)
+
+        cv2.putText(frame, str(track_count),(50, 50),0, 5e-3 * 200, (0,255,0),2)
 
         if writeVideo_flag: # and not asyncVideo_flag:
             # save a frame
@@ -109,7 +114,7 @@ def main(yolo):
 
         fps = (fps + (1./(time.time()-t1))) / 2
         print("FPS = %f"%(fps))
-        
+
         # Press Q to stop!
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -124,8 +129,6 @@ def main(yolo):
 
     if writeVideo_flag:
         out.release()
-
-    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main(YOLO())
