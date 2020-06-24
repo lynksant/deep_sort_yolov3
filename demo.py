d98ff3b2e9b74a80555b719f6460a9b3a6ee79cc
Leonid Kahanovskyi <lynksant@gmail.com>
2020-06-24 00:56:11 -0700

    Remove frame limit

--- demo.py
+++ demo.py
@@ -73,7 +73,7 @@ def main(yolo):
 
         while True:
             ret, frame = video_capture.read()  # frame shape 640*480*3
-            if ret != True or frame_index == 10:
+            if ret != True:
                  break
 
             t1 = time.time()
